40136c9d1ef34ec88ca17a3879fc32f29760e76c
Napoleon Guardado <napo.gv@hotmail.com>
2018-01-04 03:43:11 +0000

    automatically backed up by learn

--- hoisting.js
+++ hoisting.js
@@ -13,11 +13,13 @@ function callMe() {
 
 function crazy() {
   // fix the code in here:
-  thisIsCrazy();
-
   var thisIsCrazy = function (){
     console.log("hey!!!")
   }
+
+  thisIsCrazy();
+
+
 }
 
 function sayMyName() {
